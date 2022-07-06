4bc6d0ea08f23cea5a8990c1e9b3a3ec4145eca3
lindsay <lindsaychognon@gmail.com>
2022-07-06 14:21:19 +0200

    Ajout de jour_fin dans Disponibilite et modification de jour en jour_debut

--- src/Entity/Jour.php
+++ src/Entity/Jour.php
@@ -16,25 +16,32 @@ class Jour
     private $id;
 
     #[ORM\Column(type: 'date')]
-    private $jour;
+    private $jour_debut;
 
     #[ORM\ManyToOne(targetEntity: Prestation::class, inversedBy: 'jours')]
     #[ORM\JoinColumn(nullable: false)]
     private $prestation;
+
+    #[ORM\Column(type: 'date')]
+    private $jour_fin;
+
+    #[ORM\OneToOne(inversedBy: 'jour', targetEntity: Disponibilites::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $disponibilite;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getJour(): ?\DateTimeInterface
+    public function getJour_debut(): ?\DateTimeInterface
     {
-        return $this->jour;
+        return $this->jour_debut;
     }
 
-    public function setJour(\DateTimeInterface $jour): self
+    public function setJour_debut(\DateTimeInterface $jour_debut): self
     {
-        $this->jour = $jour;
+        $this->jour_debut = $jour_debut;
 
         return $this;
     }
@@ -47,6 +54,30 @@ class Jour
     public function setPrestation(?Prestation $prestation): self
     {
         $this->prestation = $prestation;
+
+        return $this;
+    }
+
+    public function getJourFin(): ?\DateTimeInterface
+    {
+        return $this->jour_fin;
+    }
+
+    public function setJourFin(\DateTimeInterface $jour_fin): self
+    {
+        $this->jour_fin = $jour_fin;
+
+        return $this;
+    }
+
+    public function getDisponibilite(): ?Disponibilites
+    {
+        return $this->disponibilite;
+    }
+
+    public function setDisponibilite(Disponibilites $disponibilite): self
+    {
+        $this->disponibilite = $disponibilite;
 
         return $this;
     }
